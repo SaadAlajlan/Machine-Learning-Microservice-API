6a70f00f35e765a52f2317347e96b8f88e8da9bc
SaadALajlan <s.alajlan10@gmail.com>
2022-05-21 00:21:51 +0300

    final commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
 dockerpath="saadaj/mlproj"
-# Step 2
-# Run the Docker Hub container with kubernetes
-kubectl run mlmicroserviceapi\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=mlmicroserviceapi
 
-# Step 3:
+# Run in Docker Hub container with kubernetes
+kubectl run --image=$dockerpath mlprojdemo --port=80 --labels app=mlprojemo
+
 # List kubernetes pods
 kubectl get pods
-# Step 4:
-# Forward the container port to a host
-kubectl port-forward mlmicroserviceapi 8000:80
+
+# Forward the container port to host
+kubectl port-forward mlprojdemo 8000:80
